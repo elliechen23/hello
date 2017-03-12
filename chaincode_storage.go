b1d7aa67b61aaebb582caa5fc00416a1bc4f4746
Ellie Chen <elliechen@example.com>
2017-03-12 09:37:44 +0800

    storage test

---
+++ chaincode_storage.go
@@ -0,0 +1,146 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
+//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
+//to be modified as well with the new ID of chaincode_example02.
+//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
+//hard-coding.
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type StorageChaincode struct {
+}
+
+func (t *StorageChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("StorageChaincode Init")
+
+	return nil, nil
+}
+
+func (t *StorageChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("StorageChaincode Invoke")
+	
+
+	if function != "invoke" {
+		return nil, errors.New("Unknown function call")
+	}
+
+	if len(args) < 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting at least 2")
+	}
+
+	if args[0] == "delete" {
+		// Deletes an entity from its state
+		return t.delete(stub, args)
+	}
+
+	if args[0] == "query" {
+		// queries an entity state
+		return t.query(stub, args)
+	}
+	if args[0] == "put" {
+		// Deletes an entity from its state
+		return t.put(stub, args)
+	}
+
+	return nil, errors.New("Invalid invoke function name. Expecting \"put\" \"delete\" \"query\"")
+}
+
+// Transaction makes payment of X units from A to B
+func (t *StorageChaincode) put(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	var A string    // Entities
+	var Aval string // Asset holdings
+	var err error
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+	
+
+	A = args[1]
+	Aval = args[2]
+
+	// Write the state back to the ledger
+	err = stub.PutState(A, []byte(Aval))
+	if err != nil {
+		return nil, err
+	}
+	
+	return nil, nil
+
+}
+
+// Deletes an entity from state
+func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	A := args[1]
+	
+	// Delete the key from the state in ledger
+	err := stub.DelState(A)
+	if err != nil {
+		return nil, errors.New("Failed to delete state")
+	}
+
+	return nil, nil
+}
+
+// query callback representing the query of a chaincode
+func (t *StorageChaincode) query(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var A string // Entities
+	var err error
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+	}
+
+	A = args[1]
+
+	// Get the state from the ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Key\":\"" + A + "\",\"Value\":\"" + string(Avalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return Avalbytes, nil
+}
+
+func main() {
+	err := shim.Start(new(StorageChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
